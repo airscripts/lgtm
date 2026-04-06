488244e77dafc1326a324a318b52cf8251bf28af
Francesco Sardone <francesco@airscript.it>
2026-04-06 13:22:30 +0200

    fix: add missing tags for og to work

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lgtm.airscript.it',
   output: 'static',
   integrations: [react()],
   vite: { plugins: [tailwindcss()] },
